aef98fcc905194ced3a50136009c814389d54154
Max Mackie <maxmackie13@gmail.com>
2021-11-06 20:02:28 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React, { Component } from "react";
+import { Greeting } from "./components/Greeting";
+
+function App() {
+  return (
+    <>
+      <Greeting />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Greeting.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Greeting = () => {
+  let phrase = "Hello";
+
+  return <h2 id="greeting">We just called to say to say {phrase}!</h2>;
+};
+
+export { Greeting };
