5f3958b379d8b19efddd58fc6c8800215e28fd2c
Max Mackie <maxmackie13@gmail.com>
2021-11-07 13:50:22 +0000

    adding starting templates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-// App.js
-import React, { Component } from "react";
-import { Greeting } from "./components/Greeting";
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+import cartItems from "./cart-items";
+// redux stuff
 
 function App() {
+  // cart setup
+
   return (
-    <>
-      <Greeting />
-    </>
+    <main>
+      <Navbar />
+      <CartContainer cart={cartItems} />
+    </main>
   );
 }
 
