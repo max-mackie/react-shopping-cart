5e4fa145eede4c10ddf12f5b0505188356701ebd
Max Mackie <maxmackie13@gmail.com>
2021-11-07 15:08:50 +0000

    add context.js

--- src/components/Greeting.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Greeting = () => {
-  let phrase = "Hello";
-
-  return <h2 id="greeting">We just called to say to say {phrase}!</h2>;
-};
-
-export { Greeting };
---
+++ src/context.js
@@ -0,0 +1,29 @@
+import React, { useState, useContext, useReducer, useEffect } from "react";
+import cartItems from "./data";
+import reducer from "./reducer";
+
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-useReducer-cart-project";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <AppContext.Provider
+      value={{
+        cart,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
